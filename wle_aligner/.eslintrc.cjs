35697bad305bb39e4eed7c66acac5537893ed5a6
Elia <eliaducceschi@hotmail.it>
2023-12-29 10:49:16 +0100

    Add packages and initial stuff

---
+++ wle_aligner/.eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        project: "tsconfig.json",
+        tsconfigRootDir: __dirname
+    },
+    env: {
+        browser: true
+    },
+    globals: {
+    },
+    plugins: [
+        "deprecation"
+    ],
+    extends: [
+        "eslint:recommended"
+    ],
+    rules: {
+        "semi": "error",
+        "no-unused-vars": ["error", { "args": "none", "varsIgnorePattern": "^__" }],
+        "deprecation/deprecation": "error"
+    },
+    ignorePatterns: [
+        "/node_modules/",
+        ".eslintrc.cjs"
+    ],
+    overrides: [
+    ]
+};

29b669ddd85dfd0ae78ca5b063cae56e9b877cc9
Elia <eliaducceschi@hotmail.it>
2024-01-12 09:59:38 +0100

    Filter by resources

--- wle_aligner/common/src/wle_aligner/align_projects.ts
+++ wle_aligner/common/src/wle_aligner/align_projects.ts
@@ -6,4 +6,22 @@ import { ProcessReport } from "./process_report.js";
 
 export async function alignProjects(sourceProject: Project, targetProject: Project, projectComponentsDefinitions: Map<string, ModifiedComponentPropertyRecord>, commanderOptions: Record<string, string>, processReport: ProcessReport) {
 
+    let changedSomething = false;
+    do {
+        if (commanderOptions.filter == null || commanderOptions.filter.indexOf("objects") >= 0) {
+            changedSomething = changedSomething || _alignObjects();
+        }
+
+    } while (changedSomething);
+}
+
+
+
+// PRIVATE
+
+function _alignObjects(): boolean {
+    const changedSomething = false;
+
+    return changedSomething;
+
 }
